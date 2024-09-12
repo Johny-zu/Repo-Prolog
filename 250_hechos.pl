1e84f3b264173d625695da15e7461aaddf63fae7
John <133547897+Johny-zu@users.noreply.github.com>
2024-09-12 13:32:53 -0600

    Ingreso de 300 hechos

---
+++ 250_hechos.pl
@@ -0,0 +1,345 @@
+%Hechos de un argumento
+% Naturaleza(Nombre).
+naturaleza(fuerte).
+naturaleza(huraña).
+naturaleza(audaz).
+naturaleza(firme).
+naturaleza(picara).
+naturaleza(osada).
+naturaleza(docil).
+naturaleza(placida).
+naturaleza(agitada).
+naturaleza(floja).
+naturaleza(miedosa).
+naturaleza(activa).
+naturaleza(seria).
+naturaleza(alegre).
+naturaleza(ingenua).
+naturaleza(modesta).
+naturaleza(afable).
+naturaleza(mansa).
+naturaleza(tímida).
+naturaleza(alocada).
+naturaleza(serena).
+naturaleza(amable).
+naturaleza(grosera).
+naturaleza(cauta).
+naturaleza(rara).
+
+%Grupo_huevo(tipo).
+Grupo_huevo(desconocido).
+Grupo_huevo(ditto).
+Grupo_huevo(planta).
+Grupo_huevo(bicho).
+Grupo_huevo(Volador).
+Grupo_huevo(Humanoide).
+Grupo_huevo(Mineral).
+Grupo_huevo(Amorfo).
+Grupo_huevo(Campo).
+Grupo_huevo(Agua 1).
+Grupo_huevo(Agua 2).
+Grupo_huevo(Agua 3).
+Grupo_huevo(Mounstro).
+Grupo_huevo(Hada).
+Grupo_huevo(Dragon).
+
+%Tipos_pokemon(tipo)
+Tipo_pokemon(normal).
+Tipo_pokemon(lucha).
+Tipo_pokemon(Volador).
+Tipo_pokemon(Bicho).
+Tipo_pokemon(Veneno).
+Tipo_pokemon(Tierra).
+Tipo_pokemon(Roca).
+Tipo_pokemon(fantasma).
+Tipo_pokemon(acero).
+Tipo_pokemon(Agua).
+Tipo_pokemon(Fuego).
+Tipo_pokemon(planta).
+Tipo_pokemon(Electrico).
+Tipo_pokemon(psiquico).
+Tipo_pokemon(hielo).
+Tipo_pokemon(Dragon).
+Tipo_pokemon(hada).
+Tipo_pokemon(Siniestro).
+
+%Habitats(habitat)
+Hábitat(campo).
+Hábitat(pradera).
+Hábitat(bosque).
+Hábitat(selva).
+Hábitat(costa).
+Hábitat(pantona).
+Hábitat(agua_salada).
+Hábitat(agua_dulce).
+Hábitat(caverna).
+Hábitat(montaña).
+Hábitat(desierto).
+Hábitat(cielo).
+Hábitat(nevado).
+Hábitat(zona_artica).
+Hábitat(ciudad).
+Hábitat(singular).
+
+%Altura
+Altura(0m).
+Altura(1m).
+Altura(2m).
+Altura(3m).
+Altura(4m).
+Altura(5m).
+Altura(6m).
+Altura(7m).
+Altura(8m).
+Altura(9m).
+Altura(10m).
+Altura(14m).
+
+%Pokemon_Iniciales_planta
+Pokemon_iniciales_planta(bulbasaur).
+Pokemon_iniciales_planta(Chikorita)
+Pokemon_iniciales_planta(Treeko).
+Pokemon_iniciales_planta(turtwig).
+Pokemon_iniciales_planta(Snivy).
+Pokemon_iniciales_planta(Chespin).
+Pokemon_iniciales_planta(Rowlett).
+Pokemon_iniciales_planta(Gooky).
+Pokemon_iniciales_planta(Spiritgatito).
+
+%pokemon_iniciales_fuego
+Pokemon_iniciales_fuego(Charmander).
+Pokemon_iniciales_fuego(Cindaquyl).
+Pokemon_iniciales_fuego(torchik).
+Pokemon_iniciales_fuego(Chimchar).
+Pokemon_iniciales_fuego(Torchik).
+Pokemon_iniciales_fuego(Fenekin).
+Pokemon_iniciales_fuego(litten).
+Pokemon_iniciales_fuego(Scorbunny).
+Pokemon_iniciales_fuego(Fuecoco).
+ 
+% NaturalezaAumenta(Naturaleza, EstadísticaAumentada).
+naturaleza_aumenta(fuerte, ataque).
+naturaleza_aumenta(huraña, ataque).
+naturaleza_aumenta(audaz, ataque).
+naturaleza_aumenta(firme, ataque).
+naturaleza_aumenta(pícara, ataque).
+naturaleza_aumenta(osada, defensa).
+naturaleza_aumenta(dócil, defensa).
+naturaleza_aumenta(plácida, defensa).
+naturaleza_aumenta(agitada, defensa).
+naturaleza_aumenta(floja, defensa).
+naturaleza_aumenta(miedosa, velocidad).
+naturaleza_aumenta(activa, velocidad).
+naturaleza_aumenta(seria, velocidad).
+naturaleza_aumenta(alegre, velocidad).
+naturaleza_aumenta(ingenua, velocidad).
+naturaleza_aumenta(modesta, ataque_especial).
+naturaleza_aumenta(afable, ataque_especial).
+naturaleza_aumenta(mansa, ataque_especial).
+naturaleza_aumenta(tímida, ataque_especial).
+naturaleza_aumenta(alocada, ataque_especial).
+naturaleza_aumenta(serena, defensa_especial).
+naturaleza_aumenta(amable, defensa_especial).
+naturaleza_aumenta(grosera, defensa_especial).
+naturaleza_aumenta(cauta, defensa_especial).
+naturaleza_aumenta(rara, defensa_especial).
+
+% NaturalezaDisminuye(Naturaleza, EstadísticaDisminuida).
+naturaleza_disminuye(fuerte, ata Pokemon_inicialesplanta().que).
+naturaleza_disminuye(huraña, defensa).
+naturaleza_disminuye(audaz, velocidad).
+naturaleza_disminuye(firme, ataque_especial).
+naturaleza_disminuye(pícara, defensa_especial).
+naturaleza_disminuye(osada, ataque).
+naturaleza_disminuye(dócil, defensa).
+naturaleza_disminuye(plácida, velocidad).
+naturaleza_disminuye(agitada, ataque_especial).
+naturaleza_disminuye(floja, defensa_especial).
+naturaleza_disminuye(miedosa, ataque).
+naturaleza_disminuye(activa, defensa).
+naturaleza_disminuye(seria, velocidad).
+naturaleza_disminuye(alegre, ataque_especial).
+naturaleza_disminuye(ingenua, defensa_especial).
+naturaleza_disminuye(modesta, ataque).
+naturaleza_disminuye(afable, defensa).
+naturaleza_disminuye(mansa, velocidad).
+naturaleza_disminuye(tímida, ataque_especial).
+naturaleza_disminuye(alocada, defensa_especial).
+naturaleza_disminuye(serena, ataque).
+naturaleza_disminuye(amable, defensa).
+naturaleza_disminuye(grosera, velocidad).
+naturaleza_disminuye(cauta, ataque_especial).
+naturaleza_disminuye(rara, defensa_especial).
+
+%Mega_evolucion(nombre_pokemon_Mega)
+Mega_evolucion(Venasaur, Mega_Vensour).
+Mega_evolucion(Charizard, Mega_Charizard_X).
+Mega_evolucion(Charizar, Mega_charizard_y).
+Mega_evolucion(Blastois, Mega_Blastois).
+Mega_evolucion(Alakazam, Mega_Alakasam).
+Mega_evolucion(Gengar, Mega_Gengar).
+Mega_evolucion(kangaskhan, Mega_Kangaskhan).
+Mega_evolucion(pinsir, Mega_pinsir).
+Mega_evolucion(Gyarados, Mega_Gyarados).
+Mega_evolucion(Aerodactyl, Mega_Aerodactil).
+Mega_evolucion(Mewtwo, Mega_mewtwo).
+Mega_evolucion(Ampharos, Mega_Ampharos).
+Mega_evolucion(Steelix, Mega_Steelix).
+Mega_evolucion(Scizor, Mega_Scizor).
+Mega_evolucion(Heracross, Mega_Heracross).
+Mega_evolucion(Houndoom, Mega_Houndoom).
+Mega_evolucion(Tyranitar, Mega_Tyranitar).
+Mega_evolucion(Sceptile, Mega_Sceptile).
+Mega_evolucion(Blaziken, Mega_Blaziken).
+Mega_evolucion(Swampert, Mega_Swampert).
+Mega_evolucion(Gardevoir, Mega_Gardevoir).
+Mega_evolucion(Sableye, Mega_Sableye).
+Mega_evolucion(Mawile, Mega_Mawile).
+Mega_evolucion(Aggron, Mega_Aggron).
+Mega_evolucion(Medicham, Mega_Medicham).
+Mega_evolucion(Manectric, Mega_Manectric).
+Mega_evolucion(Banette, Mega_Banette).
+Mega_evolucion(Absol, Mega_Absol).
+Mega_evolucion(Glalie, Mega_Glalie).
+Mega_evolucion(Salamence, Mega_Salamence).
+Mega_evolucion(Metagross, Mega_Metagross).
+Mega_evolucion(Latias, Mega_Latias).
+Mega_evolucion(Latios, Mega_Latios).
+Mega_evolucion(Kyogre, Mega_Kyogre).
+Mega_evolucion(Groudon, Mega_Groudon).
+Mega_evolucion(Rayquaza, Mega_Rayquaza).
+Mega_evolucion(Lopunny, Mega_Lopunny).
+Mega_evolucion(Garchomp, Mega_Garchomp).
+Mega_evolucion(Lucario, Mega_Lucario).
+Mega_evolucion(Abomasnow, Mega_Abomasnow).
+Mega_evolucion(Gallade, Mega_Gallade).
+Mega_evolucion(Audino, Mega_Audino).
+Mega_evolucion(Diancie, Mega_Diancie).
+
+% grupo_evolutivi sin evolucion
+Grupo_evolutivo_1(miltank, no_evoluciona).
+Grupo_evolutivo_1(lapras, no_evoluciona).
+Grupo_evolutivo_1(tauros, no_evoluciona).
+Grupo_evolutivo_1(ditto, no_evoluciona).
+Grupo_evolutivo_1(shukle, no_evoluciona).
+Grupo_evolutivo_1(deliberd, no_evoluciona).
+Grupo_evolutivo_1(skarmory, no_evoluciona).
+Grupo_evolutivo_1(smeargle, no_evoluciona).
+Grupo_evolutivo_1(Mawile, no_evoluciona).
+Grupo_evolutivo_1(sableye, no_evoluciona).
+Grupo_evolutivo_1(spiritomb, no_evoluciona).
+Grupo_evolutivo_1(heatran, no_evoluciona).
+Grupo_evolutivo_1(regigigas, no_evoluciona).
+Grupo_evolutivo_1(giratina, no_evoluciona).
+Grupo_evolutivo_1(cresselia, no_evoluciona).
+Grupo_evolutivo_1(torkoal, no_evoluciona).
+Grupo_evolutivo_1(spinda, no_evoluciona).
+
+ 
+% NaturalezaGustos(Naturaleza, GustoPreferido, GustoDisgustado).
+naturaleza_gustos(fuerte, picante, picante).
+naturaleza_gustos(huraña, picante, ácido).
+naturaleza_gustos(audaz, picante, dulce).
+naturaleza_gustos(firme, picante, seco).
+naturaleza_gustos(pícara, picante, amargo).
+naturaleza_gustos(osada, ácido, picante).
+naturaleza_gustos(dócil, ácido, ácido).
+naturaleza_gustos(plácida, ácido, dulce).
+naturaleza_gustos(agitada, ácido, seco).
+naturaleza_gustos(floja, ácido, amargo).
+naturaleza_gustos(miedosa, dulce, picante).
+naturaleza_gustos(activa, dulce, ácido).
+naturaleza_gustos(seria, dulce, dulce).
+naturaleza_gustos(alegre, dulce, seco).
+naturaleza_gustos(ingenua, dulce, amargo).
+naturaleza_gustos(modesta, seco, picante).
+naturaleza_gustos(afable, seco, ácido).
+naturaleza_gustos(mansa, seco, dulce).
+naturaleza_gustos(tímida, seco, seco).
+naturaleza_gustos(alocada, seco, amargo).
+naturaleza_gustos(serena, amargo, picante).
+naturaleza_gustos(amable, amargo, ácido).
+naturaleza_gustos(grosera, amargo, dulce).
+naturaleza_gustos(cauta, amargo, seco).
+naturaleza_gustos(rara, amargo, amargo).
+
+% NaturalezaEfectos(Naturaleza, EstadísticaAumentada, EstadísticaDisminuida, GustoPreferido).
+naturaleza_efectos(fuerte, ataque, ataque, picante).
+naturaleza_efectos(huraña, ataque, defensa, picante).
+naturaleza_efectos(audaz, ataque, velocidad, picante).
+naturaleza_efectos(firme, ataque, ataque_especial, picante).
+naturaleza_efectos(pícara, ataque, defensa_especial, picante).
+naturaleza_efectos(osada, defensa, ataque, ácido).
+naturaleza_efectos(dócil, defensa, defensa, ácido).
+naturaleza_efectos(plácida, defensa, velocidad, ácido).
+naturaleza_efectos(agitada, defensa, ataque_especial, ácido).
+naturaleza_efectos(floja, defensa, defensa_especial, ácido).
+naturaleza_efectos(miedosa, velocidad, ataque, dulce).
+naturaleza_efectos(activa, velocidad, defensa, dulce).
+naturaleza_efectos(seria, velocidad, velocidad, dulce).
+naturaleza_efectos(alegre, velocidad, ataque_especial, dulce).
+naturaleza_efectos(ingenua, velocidad, defensa_especial, dulce).
+naturaleza_efectos(modesta, ataque_especial, ataque, seco).
+naturaleza_efectos(afable, ataque_especial, defensa, seco).
+naturaleza_efectos(mansa, ataque_especial, velocidad, seco).
+naturaleza_efectos(tímida, ataque_especial, ataque_especial, seco).
+naturaleza_efectos(alocada, ataque_especial, defensa_especial, seco).
+naturaleza_efectos(serena, defensa_especial, ataque, amargo).
+naturaleza_efectos(amable, defensa_especial, defensa, amargo).
+naturaleza_efectos(grosera, defensa_especial, velocidad, amargo).
+naturaleza_efectos(cauta, defensa_especial, ataque_especial, amargo).
+naturaleza_efectos(rara, defensa_especial, defensa_especial, amargo).
+
+% NaturalezaDetallada(Naturaleza, EstadísticaAumentada, EstadísticaDisminuida, GustoPreferido, GustoDisgustado).
+naturaleza_detallada(fuerte, ataque, ataque, picante, picante).
+naturaleza_detallada(huraña, ataque, defensa, picante, ácido).
+naturaleza_detallada(audaz, ataque, velocidad, picante, dulce).
+naturaleza_detallada(firme, ataque, ataque_especial, picante, seco).
+naturaleza_detallada(pícara, ataque, defensa_especial, picante, amargo).
+naturaleza_detallada(osada, defensa, ataque, ácido, picante).
+naturaleza_detallada(dócil, defensa, defensa, ácido, ácido).
+naturaleza_detallada(plácida, defensa, velocidad, ácido, dulce).
+naturaleza_detallada(agitada, defensa, ataque_especial, ácido, seco).
+naturaleza_detallada(floja, defensa, defensa_especial, ácido, amargo).
+naturaleza_detallada(miedosa, velocidad, ataque, dulce, picante).
+naturaleza_detallada(activa, velocidad, defensa, dulce, ácido).
+naturaleza_detallada(seria, velocidad, velocidad, dulce, dulce).
+naturaleza_detallada(alegre, velocidad, ataque_especial, dulce, seco).
+naturaleza_detallada(ingenua, velocidad, defensa_especial, dulce, amargo).
+naturaleza_detallada(modesta, ataque_especial, ataque, seco, picante).
+naturaleza_detallada(afable, ataque_especial, defensa, seco, ácido).
+naturaleza_detallada(mansa, ataque_especial, velocidad, seco, dulce).
+naturaleza_detallada(tímida, ataque_especial, ataque_especial, seco, seco).
+naturaleza_detallada(alocada, ataque_especial, defensa_especial, seco, amargo).
+naturaleza_detallada(serena, defensa_especial, ataque, amargo, picante).
+naturaleza_detallada(amable, defensa_especial, defensa, amargo, ácido).
+naturaleza_detallada(grosera, defensa_especial, velocidad, amargo, dulce).
+naturaleza_detallada(cauta, defensa_especial, ataque_especial, amargo, seco).
+naturaleza_detallada(rara, defensa_especial, defensa_especial, amargo, amargo).
+
+% NaturalezaCompleta(Naturaleza, EstadísticaAumentada, EstadísticaDisminuida, GustoPreferido, GustoDisgustado, FormaToxicidad).
+naturaleza_completa(fuerte, ataque, ataque, picante, picante, forma_aguda).
+naturaleza_completa(huraña, ataque, defensa, picante, ácido, forma_grave).
+naturaleza_completa(audaz, ataque, velocidad, picante, dulce, forma_aguda).
+naturaleza_completa(firme, ataque, ataque_especial, picante, seco, forma_aguda).
+naturaleza_completa(pícara, ataque, defensa_especial, picante, amargo, forma_aguda).
+naturaleza_completa(osada, defensa, ataque, ácido, picante, forma_grave).
+naturaleza_completa(dócil, defensa, defensa, ácido, ácido, forma_aguda).
+naturaleza_completa(plácida, defensa, velocidad, ácido, dulce, forma_grave).
+naturaleza_completa(agitada, defensa, ataque_especial, ácido, seco, forma_aguda).
+naturaleza_completa(floja, defensa, defensa_especial, ácido, amargo, forma_aguda).
+naturaleza_completa(miedosa, velocidad, ataque, dulce, picante, forma_grave).
+naturaleza_completa(activa, velocidad, defensa, dulce, ácido, forma_aguda).
+naturaleza_completa(seria, velocidad, velocidad, dulce, dulce, forma_grave).
+naturaleza_completa(alegre, velocidad, ataque_especial, dulce, seco, forma_aguda).
+naturaleza_completa(ingenua, velocidad, defensa_especial, dulce, amargo, forma_aguda).
+naturaleza_completa(modesta, ataque_especial, ataque, seco, picante, forma_grave).
+naturaleza_completa(afable, ataque_especial, defensa, seco, ácido, forma_grave).
+naturaleza_completa(mansa, ataque_especial, velocidad, seco, dulce, forma_grave).
+naturaleza_completa(tímida, ataque_especial, ataque_especial, seco, seco, forma_aguda).
+naturaleza_completa(alocada, ataque_especial, defensa_especial, seco, amargo, forma_aguda).
+naturaleza_completa(serena, defensa_especial, ataque, amargo, picante, forma_aguda).
+naturaleza_completa(amable, defensa_especial, defensa, amargo, ácido, forma_aguda).
+naturaleza_completa(grosera, defensa_especial, velocidad, amargo, dulce, forma_aguda).
+naturaleza_completa(cauta, defensa_especial, ataque_especial, amargo, seco, forma_grave).
+naturaleza_completa(rara, defensa_especial, defensa_especial, amargo, amargo, forma_aguda).
